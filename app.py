d1fd70b1d5ebd4cff02359934327f2c110ba9c76
sethdow <seth.dow@gmail.com>
2021-10-06 21:57:06 +0200

    Added Navigation and meal landing page. Made recipes object globally available

--- app.py
+++ app.py
@@ -4,25 +4,35 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+
 bootstrap = flask_bootstrap.Bootstrap(app)
 with open('static/recipes.yml', 'r') as stream:
     recipes = yaml.load(stream)
 
+# add the recipes dict as a globally available variable to the templates
+@app.context_processor
+def inject_stage_and_region():
+    return dict(recipes=recipes)
 print(recipes)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/user/<name>')
 def user(name):
     myDict = {'name':name}
     print(myDict)
-    return render_template('user.html',myDict = myDict)
+    return render_template('user.html', myDict = myDict)
 
-@app.route('/recipe/<meal>/<name>')
-def recipe(meal,name):
-    return render_template('recipe.html',meal=meal,name=name, recipes=recipes)
+@app.route('/<meal>')
+def meal_choice(meal):
+    return render_template('meal.html', meal=meal)
+
+@app.route('/<meal>/<name>')
+def recipe(meal, name):
+    return render_template('recipe.html', meal=meal, name=name)
 
 if __name__ == '__main__':
     app.run()
